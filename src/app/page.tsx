ab4fe6834fb34bfda197402d5a6aff94107a333c
Sajid Hossain <bmsajidhossain420@gmail.com>
2024-09-10 22:29:18 +0600

    auth sign in and sign up page setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { AuthScreen } from "@/features/auth/components/auth-screen";
+
 export default function Home() {
-  return <div className="p-2 pt-32 m-4 font-bold">Hello</div>;
+  return <AuthScreen />;
 }
---
+++ src/features/auth/components/sign-in-card.tsx
@@ -0,0 +1,3 @@
+export const SignInCard = ({}) => {
+  return <div>Sign in</div>;
+};

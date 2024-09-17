9553c65168ee0c37ee3d2ff010a4e07c419853e3
Sajid Hossain <bmsajidhossain420@gmail.com>
2024-09-17 21:11:15 +0600

    complete sign in ui and functionalities to toggle between sign in and sign up

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -7,8 +7,16 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+import { FaGithub } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
+import { SignInFlow } from "../types";
 
-export const SignInCard = ({}) => {
+interface SignInCardProps {
+  setState: (state: SignInFlow) => void;
+}
+
+export const SignInCard = ({ setState }: SignInCardProps) => {
   return (
     <Card className="w-full h-full p-8">
       <CardHeader className="px-0 pt-0">
@@ -43,6 +51,42 @@ export const SignInCard = ({}) => {
             Continue
           </Button>
         </form>
+
+        <Separator />
+
+        <div className="flex flex-col gap-y-2.5">
+          <Button
+            disabled={false}
+            onClick={() => {}}
+            variant="outline"
+            size="lg"
+            className="w-full relative"
+          >
+            <FcGoogle className="size-5 absolute left-2.5 top-2.5" />
+            Continue with Google
+          </Button>
+
+          <Button
+            disabled={false}
+            onClick={() => {}}
+            variant="outline"
+            size="lg"
+            className="w-full relative"
+          >
+            <FaGithub className="size-5 absolute left-2.5 top-2.5" />
+            Continue with Github
+          </Button>
+        </div>
+
+        <p className="text-xs text-muted-foreground">
+          Don&apos;t have an account?{" "}
+          <span
+            onClick={() => setState("signUp")}
+            className="text-sky-700 hover:underline cursor-pointer"
+          >
+            Sign up
+          </span>
+        </p>
       </CardContent>
     </Card>
   );
--- src/features/auth/components/sign-up-card.tsx
+++ src/features/auth/components/sign-up-card.tsx
@@ -1,3 +1,9 @@
-export const SignUpCard = ({}) => {
+import { SignInFlow } from "../types";
+
+interface SignUpCardProps {
+  setState: (state: SignInFlow) => void;
+}
+
+export const SignUpCard = ({ setState }: SignUpCardProps) => {
   return <div>Sign up</div>;
 };

87782ef2466869175f76e874c38ae74698cba502
Sajid Hossain <bmsajidhossain420@gmail.com>
2024-10-10 20:40:05 +0600

    modifying the middleware so that it goes to home after logging in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 import { AuthScreen } from "@/features/auth/components/auth-screen";
 
 export default function Home() {
-  return <AuthScreen />;
+  return (
+    <div className="bg-black flex items-center justify-center h-screen text-white text-5xl font-bold underline underline-offset-4 italic">
+      Home sweet home!
+    </div>
+  );
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -12,6 +12,9 @@ export default convexAuthNextjsMiddleware((request) => {
     return nextjsMiddlewareRedirect(request, "/auth");
   }
 
+  if (isPublicPage(request) && isAuthenticatedNextjs()) {
+    return nextjsMiddlewareRedirect(request, "/");
+  }
   //TODO: redirect user away from '/auth if authenticated
 });
 

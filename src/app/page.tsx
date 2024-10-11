b57c2ddad8f1a066c1c992ddf8ebf13f694d790e
Sajid Hossain <bmsajidhossain420@gmail.com>
2024-10-11 20:05:45 +0600

    done the authentication with google

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
-import { AuthScreen } from "@/features/auth/components/auth-screen";
+"use client";
+
+import { useAuthActions } from "@convex-dev/auth/react";
 
 export default function Home() {
+  const { signOut } = useAuthActions();
   return (
-    <div className="bg-black flex items-center justify-center h-screen text-white text-5xl font-bold underline underline-offset-4 italic">
+    <div className="bg-black flex items-center justify-center flex-col h-screen text-white text-5xl font-bold underline underline-offset-4 italic">
       Home sweet home!
+      <p
+        onClick={() => signOut()}
+        className="text-lg text-blue-400 underline-offset-2 cursor-pointer"
+      >
+        Leave home :(
+      </p>
     </div>
   );
 }

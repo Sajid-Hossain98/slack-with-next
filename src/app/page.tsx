b622d77b0e57e846f1db3bc361eb5baed44d62d1
Sajid Hossain <bmsajidhossain420@gmail.com>
2024-10-24 21:16:59 +0600

    hook and api to get workspaces

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,24 @@
 "use client";
 
 import { UserButton } from "@/features/auth/components/user-button";
+import { useGetWorkspaces } from "@/features/auth/workspaces/use-get-workspaces";
+import { useEffect, useMemo } from "react";
 
 export default function Home() {
+  const { data, isLoading } = useGetWorkspaces();
+
+  const workspaceId = useMemo(() => data?.[0]?._id, [data]);
+
+  useEffect(() => {
+    if (isLoading) return;
+
+    if (workspaceId) {
+      console.log("Redirect to workspace");
+    } else {
+      console.log("Open creation modal");
+    }
+  }, [isLoading, workspaceId]);
+
   return (
     <div className="bg-black/50 h-screen">
       <UserButton />
